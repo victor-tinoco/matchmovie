d4e59e35557a0a576ee5e36ef5ce7c0fb5303551
Rafael e Davi <rafael.novais.amorim@gmail.com>
2019-11-02 14:34:05 -0300

    Adicionando tela de login

---
+++ app/src/main/java/br/com/iteris/iteris/login/ActivityLogin.java
@@ -0,0 +1,19 @@
+package br.com.iteris.iteris.login;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class ActivityLogin extends AppCompatActivity{
+
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+
+    }
+}
